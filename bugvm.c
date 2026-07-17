cd1c1ceae9e2d97fc5f1ec855100bb517bbbb491
Sviat6239 <sviatoslav6239@gmail.com>
2026-07-17 20:33:36 +0200

    fix: correct string.h include and implement initial structure for line parsing

--- bugvm.c
+++ bugvm.c
@@ -1,8 +1,30 @@
 #include <stdlib.h>
-#include <sring.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 
+typedef struct{
+    char **tokens;
+    int token_count;
+} Line;
+
+int stach[1024];
+int sp = -1;
+int ip = 0;
+
+void parse_line(const char *buffer, Line *line){
+
+}
+
 int main(){
-    printf("Init!");
+    FILE *fptr = fopen("bytecode001.bin", "r");
+    if (!fptr)
+    {
+        perror("Error opening input file 'code.as'");
+        return -1;
+    }
+
+    Line *lines = NULL;
+    int line_count = 0;
+    char buffer[1024];
 }
